38363a2a42ad85b444e488ef871d7ef0f9fe9e20
martinrusev <martinrusev@zoho.com>
2011-08-31 12:50:36 +0100

    added dynamic config file

--- amon/defaults.py
+++ amon/defaults.py
@@ -1,17 +1,27 @@
+import json
+
+try:
+	config_file = file('/etc/amon.conf').read()
+	config = json.loads(config_file)
+except:
+	config = {}
+
 #  Amon Defaults
-BACKEND = 'mongodb'
+BACKEND = config.get('backend', 'mongodb')
+
+config_mongo = config.get('mongo', {})
 
 MONGO = {
-	'port': 27017,
-	'host': 'localhost',
-	'user': '',
-	'password': '',
+	'port': config_mongo.get('port', 27017),
+	'host': config_mongo.get('host', 'localhost'),
+	'user': config_mongo.get('user', ''),
+	'password': config_mongo.get('password', ''),
 	'database' : 'amon',
 }
 
 # 1 minute default
-SYSTEM_CHECK_PERIOD = 60
+SYSTEM_CHECK_PERIOD = config.get('system_check_period', 60)
 
-SYSTEM_CHECKS = ('cpu', 'memory', 'disk', 'network', 'loadavg')
+SYSTEM_CHECKS = config.get('system_checks', ['cpu', 'memory', 'disk', 'network', 'loadavg'])
 
-PROCESS_CHECKS = ('mongo', )
+PROCESS_CHECKS = config.get('process_checks', [])
