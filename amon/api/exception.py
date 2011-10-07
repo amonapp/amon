87c7a3e514c97af5b75f19bba277b06f2731f267
martinrusev <martinrusev@zoho.com>
2011-10-07 12:52:21 +0100

    Exception grouping almost working

--- amon/api/exception.py
+++ amon/api/exception.py
@@ -1,6 +1,8 @@
 from time import time
 from amon.backends.mongodb import backend
 import json
+from hashlib import md5
+import amonpy
 
 """
  Exception structure:
@@ -16,6 +18,7 @@ class Exception(object):
 
 	def __init__(self):
 		self.exception = {}
+		self.collection = 'exceptions'
 
 	def __call__(self, *args, **kwargs):
 		
@@ -23,19 +26,39 @@ class Exception(object):
 
 		exception_dict = json.loads(args[0])
 		exception_class = exception_dict.get('exception_class', '')
-		message= exception_dict.get('message', '')
 		url = exception_dict.get('url', '')
 		backtrace = exception_dict.get('backtrace', '')
+		
+		message= exception_dict.get('message', '')
 		enviroment = exception_dict.get('enviroment', '')
 		data = exception_dict.get('data', '')
-
-		entry = {'time': now,
-				 'message': message,
-				 'url': url,
-				 'enviroment': enviroment,
-				 'backtrace' : backtrace,
-				 'data': data,
-				 'exception_class': exception_class
-				 }
 		
-		backend.store_entry(entry, 'exceptions')
+		
+		exception_string = "{0}{1}{2}".format(exception_class, url, backtrace)
+		exception_id = md5(exception_string).hexdigest()
+		amonpy.log(message=exception_id, level='info')
+		
+		
+		additional_data = {'message': message,
+						   'enviroment': enviroment,
+						   'data': data,
+						   'occurrence': now}
+
+		exceptions_collection = backend.get_collection(self.collection)
+		exception_in_db = exceptions_collection.find_one({'exception_id': exception_id})
+
+		if exception_in_db is True:
+			exception_in_db['last_occurrence'] = now
+			exception_in_db['additional_data'].insert(0, additional_data)
+
+			exceptions_collection.update({'_id' : exception_in_db['_id']}, exception_in_db)
+		else:
+			entry = {'last_occurrence': now,
+					 'exception_id': exception_id,
+					 'exception_class': exception_class,
+					 'url': url,
+					 'backtrace' : backtrace,
+					 'additional_data': [additional_data]
+					 }
+
+			backend.store_entry(entry, self.collection)
