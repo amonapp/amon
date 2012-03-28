573bc7014d09ca2113b28a1f37fcc0bfc3248394
martinrusev <martinrusev@zoho.com>
2012-03-28 15:28:58 +0100

    ZeroMQ address in the config file

--- amon/defaults.py
+++ amon/defaults.py
@@ -50,6 +50,7 @@ WEB_APP = {
 ACL = config.get('acl', "False") # Expects string
 key = config.get('secret_key', None)
 PROXY = config.get('proxy', None) # Relative baseurl if true
+ZEROMQ = config.get('zeromq', 'localhost:5464') # TCP address for the ZeroMQ daemon
 
 TIMEZONE = config.get('timezone','UTC')
 
