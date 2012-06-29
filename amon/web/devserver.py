a5283e4856c86fa3035bfbf82168f3ed40b67367
Martin Rusev <martinrusev@zoho.com>
2012-06-29 23:19:24 +0300

    SSL tests

--- amon/web/devserver.py
+++ amon/web/devserver.py
@@ -1,18 +1,23 @@
+DEV_PATH = '/home/martin/amon'
+import os
 import sys
-
-if sys.platform == 'darwin':
-    sys.path.insert(0, '/Users/User/amon')
-else:
-    sys.path.insert(0,'/home/martin/amon')
-
+sys.path.insert(0, DEV_PATH)
 
 from amon.web.server import application
 import tornado.ioloop
+import tornado.httpserver
 from amon.core import settings
 from tornado import autoreload
 
+#ssl_options={ 
+    #"certfile": os.path.join(DEV_PATH, "amon.crt"), 
+    #"keyfile": os.path.join(DEV_PATH, "amon.key"), 
+#}
+
 if __name__ == "__main__":
-	application.listen(int(settings.WEB_APP['port']))
-	ioloop = tornado.ioloop.IOLoop().instance()
-	autoreload.start(ioloop)
-	ioloop.start()
+    #http_server = tornado.httpserver.HTTPServer(application, ssl_options=ssl_options)
+    #http_server.listen(int(settings.WEB_APP['port']))
+    application.listen(int(settings.WEB_APP['port']))
+    ioloop = tornado.ioloop.IOLoop().instance()
+    autoreload.start(ioloop)
+    ioloop.start()
