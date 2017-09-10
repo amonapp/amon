999df9bf17c4222d29c4bc5427b5e26a9c8bffc1
martinrusev <martin@amon.cx>
2017-09-10 13:26:58 +0200

    Update vagrantfile

--- scripts/amonagent-vagrant/provision_deb.sh
+++ scripts/amonagent-vagrant/provision_deb.sh
@@ -5,15 +5,9 @@ echo ">>> Installing Base Packages"
 sudo apt-get update
 
 # Install base packages
-sudo apt-get install -qq curl unzip git-core ack-grep software-properties-common build-essential zsh  ruby ruby-dev vim
+sudo apt-get install -qq curl build-essential zsh vim
 
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -qq ansible
 
 # Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-sudo gem install sensu-plugins-http sensu-plugins-filesystem-checks sensu-plugins-network-checks sensu-plugins-process-checks sensu-plugins-entropy-checks sensu-plugins-disk-checks sensu-plugins-dns --no-ri --no-rdoc
-
 
