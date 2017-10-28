097cccec41d4455c73d586ef4506075f8c7c1004
Daniel Christophersen <christophersen@yay-digital.de>
2017-10-28 23:51:04 +0200

    Switch to OpsGenie API V2

--- amon/apps/notifications/opsgenie/sender.py
+++ amon/apps/notifications/opsgenie/sender.py
@@ -2,23 +2,25 @@ import requests
 import json
 
 from amon.apps.notifications.models import notifications_model
-        
+
 def send_opsgenie_notification(message=None, auth=None):
     sent = False
-    url = "https://api.opsgenie.com/v1/json/alert"
-
+    url = "https://api.opsgenie.com/v2/alerts"
+    headers = {
+        'Authorization': 'GenieKey '+ auth.get('api_key'),
+        'Content-Type': 'application/json'
+    }
 
     # Message is limited to 130 chars
     data = {
-            'apiKey': auth.get('api_key'),
             'message': message,
         }
 
     data = json.dumps(data)
     error = None
-    
+
     try:
-        r = requests.post(url, data=data, timeout=5)
+        r = requests.post(url, data=data, timeout=5, headers=headers)
     except Exception as e:
         error = e
 
